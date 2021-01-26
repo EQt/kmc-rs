1957f972781df68a44d3f4df236a97783afe6f25
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2021-01-26 12:55:40 +0100

    test py_kmc_api

---
+++ data/test_pyapi.py
@@ -0,0 +1,46 @@
+import py_kmc_api
+import pytest
+
+
+@pytest.fixture
+def io():
+    io = py_kmc_api.KMCFile()
+    assert io.OpenForRA("test1")
+    yield io
+    io.Close()
+
+
+def test_stats(io):
+    assert io.KmerLength() == 5
+    assert io.KmerCount() == 291
+
+
+def test_construct_kmer():
+    kmer = py_kmc_api.KmerAPI()
+    assert kmer.from_string("TCTTA")
+    assert kmer.to_string() == "TCTTA"
+
+
+def test_reverse_kmer():
+    kmer = py_kmc_api.KmerAPI()
+    assert kmer.from_string("TCTTA")
+    assert kmer.reverse()
+    assert kmer.to_string() == "TAAGA"
+
+
+def test_kmer_to_long():
+    kmer = py_kmc_api.KmerAPI()
+    assert kmer.from_string("TCTTA")
+    long_kmer = py_kmc_api.LongKmerRepresentation()
+    kmer.to_long(long_kmer)
+    assert long_kmer.value == [0b11_01_11_11_00]
+
+
+def test_kmer_counts(io):
+    count = py_kmc_api.Count()
+    assert count.value == 0
+    kmer = py_kmc_api.KmerAPI()
+    assert kmer.from_string("TAAGA")
+    assert len(kmer.to_string()) == io.KmerLength()
+    assert io.CheckKmer(kmer, count)
+    assert count.value == 4
