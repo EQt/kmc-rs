603197de75d855f17edab341d4f3639192b9891e
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2021-01-26 10:31:07 +0100

    fix some includes

--- src/kmc_file.cc
+++ src/kmc_file.cc
@@ -1,9 +1,6 @@
-#include "kmc-rs/include/KMC/kmc_api/kmc_file.h"
-#include "kmc-rs/src/kmc_file.h"
-
-CKMCFile::CKMCFile() {}
+#include "kmc_file.h"
 
 
 std::unique_ptr<CKMCFile> new_ckmc_file() {
-  return std::make_unique<CKMCFile>();
+    return std::make_unique<CKMCFile>();
 }
--- src/kmc_file.h
+++ src/kmc_file.h
@@ -1,6 +1,10 @@
 #ifndef FUNCTIONS_H_INCLUDED
 #define FUNCTIONS_H_INCLUDED
+#include <memory>       // for std::unique_ptr
+#include "../include/KMC/kmc_api/kmc_file.h"
+
 
 std::unique_ptr<CKMCFile> new_ckmc_file();
+
 
 #endif
